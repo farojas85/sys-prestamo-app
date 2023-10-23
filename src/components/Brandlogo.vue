d8ef46cd061882a08ca2c234c9081e0b2ef087f1
Fredy Rojas <farojas851@gmail.com>
2023-10-22 19:43:20 -0500

    fix: logo

--- src/components/Brandlogo.vue
+++ src/components/Brandlogo.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 <template>
     <a href="/dashboard" class="brand-link bg-success">
-      <img src="img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Préstamos</span>
     </a>
 </template>
